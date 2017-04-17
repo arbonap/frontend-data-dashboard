c1ccda6bd4f4210ef3b68ab14583989e085a830c
Patricia <Patricia@Patricias-MacBook-Air.local>
2017-04-16 17:42:52 -0700

    Render JSON

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+// sortedEmails = [fromEmail, toEmail].sort()
+//
+// {
+//   [`${sortedEmails[0]}:${sortedEmails[1]}`]: 1
+// }
